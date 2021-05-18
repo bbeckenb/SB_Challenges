fa4b9b52c9f57f879844dea0287228b57f770a1a
Bryce Beckenbach <brycebeckenbach@gmail.com>
2021-05-18 11:19:05 -0400

    Cleaned and organized folders/ files, completed first 8.2 exercises

--- 8_Array_Functions/8.1_filter_forEach_map/advanced_arr.js
+++ 8_Array_Functions/8.1_filter_forEach_map/advanced_arr.js
@@ -192,3 +192,27 @@ function removeVowels(str) {
     }
     return out_word;
 }
+
+//some, checks if any elements of an array meet criteria
+
+function mySome(arr, callback) {
+    for (let i = 0; i < arr.length; i++) {
+        if(callback(arr[i], i, arr)) return true;
+    }
+    return false;
+}
+
+mySome([4,5,6,7], function(n) {
+    return n > 5;
+})
+
+function myEvery(arr, callback) {
+    for (let i = 0; i < arr.length; i++) {
+        if(!callback(arr[i], i, arr)) return false;
+    }
+    return true;
+}
+
+myEvery([4,5,6,7], function(n) {
+    return Number.isInteger(n);
+})
