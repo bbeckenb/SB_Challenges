c168e8e7a4c82da6dbc33f88dbc56674120c0f60
Bryce Beckenbach <brycebeckenbach@gmail.com>
2021-05-18 14:03:19 -0400

    further organiz ed files and folders (mostly in mod 8 folders), completed 8.2 find/findIndex exercises

---
+++ 8_Array_Functions/8.2_some_every_find_findindex/js-array-find-findindex/find-findindex.js
@@ -0,0 +1,47 @@
+/* 
+Write a function called `findUserByUsername` which accepts an array of objects, each with a key of username, and a string. The function should return the first object with the key of username that matches the string passed to the function. If the object is not found, return undefined. 
+
+const users = [
+  {username: 'mlewis'},
+  {username: 'akagen'},
+  {username: 'msmith'}
+];
+
+findUserByUsername(users, 'mlewis') // {username: 'mlewis'}
+findUserByUsername(users, 'taco') // undefined
+*/
+
+
+function findUserByUsername(usersArray, username) {
+  return usersArray.find(function(e) {
+    return e['username'] === username; 
+  })
+}
+
+/*
+Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined. 
+
+const users = [
+  {username: 'mlewis'},
+  {username: 'akagen'},
+  {username: 'msmith'}
+];
+
+removeUser(users, 'akagen') // {username: 'akagen'}
+removeUser(users, 'akagen') // undefined
+*/
+
+function removeUser(usersArray, username) {
+  let index_user = usersArray.findIndex(function(e) {
+    return e['username'] === username;
+  });
+  console.log(index_user);
+  if(index_user === -1) {
+    return undefined;
+  }
+  let removed_user = usersArray[index_user];
+  usersArray.splice(index_user, 1);
+  console.log(usersArray);
+  console.log(removed_user);
+  return removed_user;
+}
