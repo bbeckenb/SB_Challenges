256853c83d525a8875d9dff6c396cb650d9a635c
Bryce Beckenbach <brycebeckenbach@gmail.com>
2021-06-06 18:53:34 -0400

    completed Python intro exercise

---
+++ 18_Python_Intro/python-syntax/convert.py
@@ -0,0 +1,33 @@
+def convert_temp(unit_in, unit_out, temp):
+    """Convert farenheit <-> celsius and return results.
+
+    - unit_in: either "f" or "c" 
+    - unit_out: either "f" or "c"
+    - temp: temperature (in f or c, depending on unit_in)
+
+    Return results of conversion, if any.
+
+    If unit_in or unit_out are invalid, return "Invalid unit [UNIT_IN]".
+
+    For example:
+
+      convert_temp("c", "f", 0)  =>  32.0
+      convert_temp("f", "c", 212) => 100.0
+    """
+
+    if unit_in is 'c':
+      temp = (temp * 9/5) + 32
+      return temp
+    elif unit_in is 'f':
+      temp = (temp - 32) * 5/9
+      return temp
+    else:
+      return "Invalid unit [UNIT_IN]"
+
+
+print("c", "f", 0, convert_temp("c", "f", 0), "should be 32.0")
+print("f", "c", 212, convert_temp("f", "c", 212), "should be 100.0")
+print("z", "f", 32, convert_temp("z", "f", 32), "should be Invalid unit z")
+print("c", "z", 32, convert_temp("c", "z", 32), "should be Invalid unit z")
+print("f", "f", 75.5, convert_temp("f", "f", 75.5), "should be 75.5")
+
