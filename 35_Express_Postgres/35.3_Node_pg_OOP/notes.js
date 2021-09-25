4570cf34a52e7e867d7870f2c1e6ef3c862a0599
Bryce Beckenbach <brycebeckenbach@gmail.com>
2021-09-25 15:16:55 -0400

    completed lunch.ly project

---
+++ 35_Express_Postgres/35.3_Node_pg_OOP/notes.js
@@ -0,0 +1,9 @@
+/**
+ * OOP - to help us organizing
+ * Encapsulation - group functionality into logical pieces
+ * Polymorphism - ability to implement similar functionality in different classes
+ * Could have other kinds of animals with the same API, but they behave slightly differently
+ * If you define the class method as 'static' you can just call the class to invoke the method, you don't need to make an instance 
+ * 
+ * Creating mini-ORM
+ */
